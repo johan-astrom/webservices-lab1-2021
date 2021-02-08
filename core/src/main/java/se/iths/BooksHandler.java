1da41d86da93383bc7cf8b73f94090b7d6bbf461
andreas nehl <andenk@hotmail.se>
2021-02-08 19:04:16 +0100

    ConvertToJson

--- core/src/main/java/se/iths/BooksHandler.java
+++ core/src/main/java/se/iths/BooksHandler.java
@@ -4,17 +4,28 @@ import se.iths.jpa.Book;
 import se.iths.jpa.BookDAO;
 import se.iths.jpa.BookDAOWithJPAImpl;
 import com.google.gson.Gson;
+
+import java.util.List;
+
 public class BooksHandler implements UrlHandler {
     @Override
     public void handlerUrl() {
 
 
         BookDAO bdao = new BookDAOWithJPAImpl();
+        List<Book> allBooks = bdao.getAllBooks();
 
-        Gson gson = new Gson();
-        String json = gson.toJson(bdao.getAllBooks());
-        System.out.println(json);
+        jsonSide(ConvertJson.convertTojson(allBooks));
 
 
     }
+
+
+
+    private void jsonSide(String json) {
+        // skapa html med json mellen body tag
+        System.out.println(json);
+    }
+
+
 }
---
+++ core/src/main/java/se/iths/ConvertJson.java
@@ -0,0 +1,18 @@
+package se.iths;
+
+import com.google.gson.Gson;
+
+import java.util.List;
+
+public class ConvertJson {
+
+    static String convertTojson(List allBooks) {
+
+        Gson gson = new Gson();
+        String json = gson.toJson(allBooks);
+
+        return json;
+
+    }
+
+}
