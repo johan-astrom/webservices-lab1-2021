cf21b2c6feba250d80825f2e4a8016bc57a40901
SofHldn <sofie.holden@iths.se>
2021-02-15 15:48:13 +0100

    försökte städa lite i viewershandler

--- plugin/src/main/java/se/iths/plugin/ViewersHandler.java
+++ plugin/src/main/java/se/iths/plugin/ViewersHandler.java
@@ -31,8 +31,6 @@ public class ViewersHandler implements StatisticsHandler {
     public HttpResponse handlerUrl(HttpRequest httpRequest) {
 
 
-
-
         StatisticsDAO sdao = new StatisticsDAOWithJPAImpl();
 
         List<Statistics> statistics = sdao.getAllStatistics();
@@ -42,7 +40,7 @@ public class ViewersHandler implements StatisticsHandler {
         for (Statistics statis : statistics) {
             statsCount.add(statis.getUrl());
         }
-        int cat = Collections.frequency(statsCount, "/cat.jpg");
+        git int cat = Collections.frequency(statsCount, "/cat.jpg");
         int index = Collections.frequency(statsCount, "/index.html");
         int books = Collections.frequency(statsCount, "/books");
         int stats = Collections.frequency(statsCount, "/stats");
@@ -53,33 +51,7 @@ public class ViewersHandler implements StatisticsHandler {
 
 
         if(httpRequest.getUrl().indexOf("?")!=-1) {
-            try {
-                URL parameter = new URL("http://localhost:7050" + httpRequest.getUrl());
-
-                type = parameter.getQuery().split("=")[1];
-                System.out.println(type);
-
-                switch (type){
-                    case "cat":
-                        httpResponse.printJsonResponse("cat viewed: " + cat + " times.");
-                        break;
-                    case "index":
-                        httpResponse.printJsonResponse("index viewed: " + index + " times.");
-                        break;
-                    case "books":
-                        httpResponse.printJsonResponse("books viewed: " + books + " times.");
-                        break;
-                    case "stats":
-                        httpResponse.printJsonResponse("stats viewed: " + stats + " times.");
-                        break;
-                    default:
-                        type = null;
-
-                }
-                System.out.println(parameter.getQuery() + "--------------------------->");
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
+            parameterReader(httpRequest, cat, index, books, stats, httpResponse);
 
         }else {
 
@@ -95,6 +67,36 @@ public class ViewersHandler implements StatisticsHandler {
 
 
         return httpResponse;
+    }
+
+    private void parameterReader(HttpRequest httpRequest, int cat, int index, int books, int stats, HttpResponse httpResponse) {
+        String type;
+        try {
+            URL parameter = new URL("http://localhost:7050" + httpRequest.getUrl());
+
+            type = parameter.getQuery().split("=")[1];
+            System.out.println(type);
+
+            switch (type){
+                case "cat":
+                    httpResponse.printJsonResponse("cat viewed: " + cat + " times.");
+                    break;
+                case "index":
+                    httpResponse.printJsonResponse("index viewed: " + index + " times.");
+                    break;
+                case "books":
+                    httpResponse.printJsonResponse("books viewed: " + books + " times.");
+                    break;
+                case "stats":
+                    httpResponse.printJsonResponse("stats viewed: " + stats + " times.");
+                    break;
+
+
+            }
+            System.out.println(parameter.getQuery() + "--------------------------->");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
